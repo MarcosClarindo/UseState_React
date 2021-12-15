0879bfa2815c3229535c6cae0c017b2a045edf36
Marco Clarindo <marcos_clarindo@hotmail.com>
2021-12-15 12:39:45 -0300

    Manipulando o Estado do Componente

---
+++ src/Counter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './Counter.css'
+import { useState } from 'react';
+
+const  Counter = () => {
+    const [contador, setContador] = useState(0)
+
+
+    const handleOnClick = operador => {
+        const novoValor = operador === '+' 
+        ? contador + 1
+        : contador - 1
+
+        
+        setContador(novoValor)
+    }
+    return(
+        <div className="counter">
+            <span>{contador}</span>
+            <button onClick={() => handleOnClick('-')}>-</button>
+            <button onClick={() =>handleOnClick('+')}>+</button>
+        </div>
+    )
+}
+
+export default Counter
